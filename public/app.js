ea32b6436474e7049024cb4befef55c1199cd786
StarfoxMulder <cjprestia@gmail.com>
2017-04-06 16:17:49 -0400

    have functioning self-made media and created llsArticle.html as a workaround for lls not allowing iframs

--- public/app.js
+++ public/app.js
@@ -2,4 +2,8 @@
 $(".featuredIcon").click(function(e){
   $(".featuredIcon").removeClass("bot-left");
   $(this).addClass("bot-left");
+
+  $("#featuredMedia").empty();
+  src = $(this).data("src");
+  $("#featuredMedia").attr("src", src);
 })

2b33f399f3ad840407b43cd7f39c8dd7647104bf
StarfoxMulder <cjprestia@gmail.com>
2017-05-07 16:54:22 -0400

    updated family page

--- public/app.js
+++ public/app.js
@@ -1,37 +1,43 @@
+window.onload = function() {
+  $(".featuredIcon").click(function(e){
+    $(".featuredIcon").removeClass("bot-left");
+    $(this).addClass("bot-left");
 
-$(".featuredIcon").click(function(e){
-  $(".featuredIcon").removeClass("bot-left");
-  $(this).addClass("bot-left");
+    $("#featuredMedia").empty();
+    src = $(this).data("src");
+    $("#featuredMedia").attr("src", src);
+  })
 
-  $("#featuredMedia").empty();
-  src = $(this).data("src");
-  $("#featuredMedia").attr("src", src);
-})
+  $("#cj").hover( function() {
+    console.log("general sytax is firing the hover");
+    $(".container").attr("background-image", "./assets/images/cjActive.gif");
+  });
 
-$(".familyMember").click(function(e){
-  $(this).addClass("active")
-  function changeImg() {
-    var name = $(this).attr("id");
-    var image = name + "Pic";
-
-
-  }
-  $(".")
-  setInterval(changeImg, 1000);
-})
+};
+// $(".familyMember").click(function(e){
+//   $(this).addClass("active")
+//   function changeImg() {
+//     var name = $(this).attr("id");
+//     var image = name + "Pic";
 
 
-$('body').on('click', 'img', function(){
-		var state = $(this).attr('data-state');
-		console.log(state);
+//   }
+//   $(".")
+//   setInterval(changeImg, 1000);
+// })
 
-		if (state == 'still') {
-			$(this).attr('data-state', 'playing');
-			$(this).attr('src', $(this).attr('data-animate'));
-		}
-		else if (state != 'still') {
-			console.log('bloop?');
-			$(this).attr('data-state', 'still');
-			$(this).attr('src', $(this).attr('data-still'));
-		}
-	});
+
+// $('body').on('click', 'img', function(){
+// 		var state = $(this).attr('data-state');
+// 		console.log(state);
+
+// 		if (state == 'still') {
+// 			$(this).attr('data-state', 'playing');
+// 			$(this).attr('src', $(this).attr('data-animate'));
+// 		}
+// 		else if (state != 'still') {
+// 			console.log('bloop?');
+// 			$(this).attr('data-state', 'still');
+// 			$(this).attr('src', $(this).attr('data-still'));
+// 		}
+// 	});
